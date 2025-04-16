9ee5c3257de3c98c88d8cd1f52b362be51127bed
Tree4-4 <trey65ts@gmail.com>
2025-04-16 09:08:59 -0500

    imporvement

--- app/daos/api/heroDao.js
+++ app/daos/api/heroDao.js
@@ -1,4 +1,4 @@
-const con = require('../../config/dbconfig')
+const con = require('../../config/dbconfig') 
 
 const heroDao = {
     table: 'hero',
@@ -25,18 +25,61 @@ const heroDao = {
     },
 
 
-    findHeroById
+    findHeroById: (res, table, id)=> {
+        con.query(
+            `SELECT h.hero_id, h.hero_name, h.first_name,
+            h.last_name, h.alias, f.franchise, s.species, 
+            h.place_of_origin, h.first_app, h.alignment, h.img_url
+            FROM hero h 
+            JOIN franchise f USING (franchise_id)
+            JOIN species s USING (species_id)
+            WHERE h.hero_id = ${id};`,
+            (error, rows) => {
+                if (!error) {
+                    if (rows.length === 1) {
+                        res.json(...rows)
+                    } else {
+                        res.json(rows)
+                    }
+                } else {
+                    console.log(`DAO ERROR: ${table}`, error)
+                }
+            }
+        )
+    },
+    
+    findbytAlignment:(res, table, aligmnet)=> {
+        con.query (
+            `SELECT h.hero_id, h.hero_name, h.first_name,
+            h.lastname, h.alias, franchise, s.species, 
+            h.place_of_origin, h.first_app, h.aligment, h.img_url
+            FROM hero h
+            JOIN franchiser f USING (franchise_id)
+            JOIN species a USING (speices_id);
+            ORDER BY h.hero_name, h.last_name, h.first_name;`,
+            (error, rows) => {
+                if (!error) {
+                    if(rows.length === 1) {
+                        res.json(...rows)
+                    } else {
+                        res.json(rows)
+                    }
+                } else {
+                    console.log(`DAO ERROR: ${table}" `, error)
+                }
+            }
+        )
+    }
 }
 
-findbytAlignment:(res, table, aligmnet)=> {
-    con.query (
-        `SELECT h.hero_id, h.hero_name, h.first_name,
-        h.lastname, h.alias, franchise, s.species, 
-        h.place_of_origin, h.first_app, h.aligment, h.img_url
-        FROM hero h
-        JOIN franchiser f USING (franchise_id)
-        JOIN species a USING (speices_id);
-        ORDER BY h.hero_name, h.last_name, h.first_name;`,
+    
+    findHeroPowers: (res, table, id)=> {
+        con.query (
+        `SELECT ${table}.hero_id, p.power FROM
+        ${table}
+        JOIN hero_to_power hp ON ${table}.hero_id = hp.hero_id
+        JOIN power p ON p.power_id = hp.power_id
+        Where ${table}.hero_id = ${id};`,
         (error, rows) => {
             if (!error) {
                 if(rows.length === 1) {
@@ -50,5 +93,6 @@ findbytAlignment:(res, table, aligmnet)=> {
         }
     )
 }
+
 
 module.exports = heroDao
--- app/routes/router.js
+++ app/routes/router.js
@@ -2,14 +2,15 @@
 // 1
 const express = require('express')
 const router = express.Router()
+const axios = require('axios')
 const PORT = process.env.PORT || 3000
 
 // 2
 router.use(express.static('public'))
 
 // individual routes 
-router.use('/api/hero', require('./api/heroRoutes'))
-router.use('/api/franchise', require('./api/franchsieRoutes'))
+// router.use('/api/hero', require('./api/heroRoutes'))
+// router.use('/api/franchise', require('./api/franchsieRoutes'))
 
 endpoints.forEach(endpoint => {
     router.use(`api/${endpoint}`, require(`./api/${endpoint}Routes`))
@@ -21,11 +22,47 @@ const endpoints = [ 'hero', 'francise', 'team', 'power', 'speices']
 //3
 // router.get(path, callback function)
 router.get('/', (req, res)=> {
-    res.render('pages/home',{
-        title: 'Home',
-        name: 'My Hero Website'
-    })
-})
+
+    const id = req.params.id
+    letheroPowers = []
+    const url = `http://localhost:${PORT}/api/hero/${id}`
+
+    const powerURL = `http://localhost:${PORT}/api/hero/${id}/power`
+
+    axios.get(url)
+    .then(resp => {
+        randomHero = resp.data[Math.floor(Math.random() * resp.data.length)]
+
+        let heroName = randomHero.hero_name != null ? randomHero.hero_name : `${randomHero.first_name} ${randomHero.last_name}`
+
+        switch (randomHero.alignment) {
+
+            case 'HERO': 
+                message = `Great news! ${heroName} is here to save you!`
+                break;
+            case 'ANTIHERO':
+                message = `I guess you need to get on ${heroName}'s good side if you want to live.`
+                break;
+            case 'VILLAIN':
+                message = `Looks like ${heroName} is here to destroy you and everything you love`
+                break;
+            default:
+                    message = ''
+                    break;
+        }
+            
+            let randomHero = {}
+            const url = `http://localhost:${PORT}/api/hero`
+            
+            res.render('pages/home',{
+                title: 'Home',
+                name: 'My Hero Website',
+                randomHero,
+                message
+            })
+        })
+    })  
+    
 
 // 1b 
 module.exports = router
